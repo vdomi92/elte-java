46ad2f49b92d28a427e175e52a143c66576af981
vataid <vatai.domonkos@szgyf.gov.hu>
2022-10-11 11:37:02 +0200

    hf 4 final

---
+++ VectorMain.java
@@ -0,0 +1,26 @@
+import utils.DoubleVector;
+
+class VectorMain{
+
+    public static void main(String[] args){
+        DoubleVector vector1 = new DoubleVector(new double[]{1.0, 1.0});
+        DoubleVector vector2 = new DoubleVector();
+        DoubleVector vector3 = new DoubleVector(new double[]{1.2, 3.3, 6.4, 5.2, 10.13});
+
+        System.out.println(vector1.multiplyByScalar(Math.PI).toString());
+        System.out.println(vector2.multiplyByScalar(Math.PI).toString());
+        System.out.println(vector3.multiplyByScalar(Math.PI).toString());
+
+        System.out.println(vector1.deduct(vector1).toString());
+        System.out.println(vector2.deduct(vector2).toString());
+        System.out.println(vector3.deduct(vector3).toString());
+
+        System.out.println(vector1.add(vector1).toString());
+        System.out.println(vector2.add(vector2).toString());
+        System.out.println(vector3.add(vector3).toString());
+
+        System.out.println(vector1.multiplyVectors(vector1).toString());
+        System.out.println(vector2.multiplyVectors(vector3).toString());
+        System.out.println(vector3.multiplyVectors(vector3).toString());        
+    }
+}
---
+++ utils/DoubleVector.java
@@ -0,0 +1,86 @@
+package utils;
+
+public class DoubleVector{
+
+    private double[] coordinates;
+
+    public DoubleVector(){
+        this.coordinates = new double[]{0,0};
+    }
+    public DoubleVector(double[] coords){
+        this.coordinates = coords;
+    }
+
+    public void setVectorCoordinates(double[] newCoordinates){
+        this.coordinates = newCoordinates;
+    }
+
+    public DoubleVector multiplyVectors(DoubleVector v){
+        
+        if(this.coordinates.length != v.coordinates.length){
+            System.out.println("Vectors must be of the same dimension");
+        }else{
+            double[] v3coords = new double[this.coordinates.length];
+            for(int i = 0; i < this.coordinates.length; i++){
+                v3coords[i] = this.coordinates[i] * v.coordinates[i];
+            }
+            this.setVectorCoordinates(v3coords);
+        }
+
+        return this;
+    }
+
+    public DoubleVector add(DoubleVector v){
+        if(this.coordinates.length != v.coordinates.length){
+            System.out.println("Vectors must be of the same dimension");
+        }else{
+            double[] v3coords = new double[this.coordinates.length];
+            for(int i = 0; i < this.coordinates.length; i++){
+                v3coords[i] = this.coordinates[i] + v.coordinates[i];
+            }
+            this.setVectorCoordinates(v3coords);
+        }
+
+        return this;
+    }
+
+    public DoubleVector deduct(DoubleVector v){
+        if(this.coordinates.length != v.coordinates.length){
+            System.out.println("Vectors must be of the same dimension");
+        }else{
+            double[] v3coords = new double[this.coordinates.length];
+            for(int i = 0; i < this.coordinates.length; i++){
+                v3coords[i] = this.coordinates[i] - v.coordinates[i];
+            }
+            this.setVectorCoordinates(v3coords);
+        }
+
+        return this;
+    }
+
+    public DoubleVector multiplyByScalar(double scalar){
+        for(double element: this.coordinates){
+            element *= scalar;
+            System.out.println(element);
+        }
+        return this;
+    }
+
+    public String toString(){
+        String str = new String();
+        str = "V(";
+        for(int i = 0; i < this.coordinates.length; i++){
+            if(i == this.coordinates.length - 1){
+                str += this.coordinates[i];
+            }else{
+                str += this.coordinates[i] + ", ";
+            }
+        }
+        str += ")";
+        return str;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
